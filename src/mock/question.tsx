1ba295118fa48760eb507de113a7afcc3e98849e
lventou <865413601@qq.com>
2023-07-11 17:33:09 +0800

    Restful API

--- src/mock/question.tsx
+++ src/mock/question.tsx
@@ -15,6 +15,65 @@ const arr = [
         },
       };
     }
+  },
+  // Get a single questionnaire
+  {
+    url: '/mock/api/question/:id',
+    methods: "get",
+    response: () => {
+      return{
+        error:0,
+        data: {
+          id: Random.id(),
+          title: Random.ctitle(5, 10),
+          description: Random.csentence(10, 20),
+        }
+      }
+    }
+  },
+  // Update questionnaire information
+  {
+    url: '/mock/api/question/:id',
+    methods: "put",
+    response: () => {
+      return{
+        error:0,
+        data: {
+          id: Random.id(),
+          title: Random.ctitle(5, 10),
+        }
+      }
+    }
+  },
+  // Capacity delete the questionnaire
+  {
+    url: '/mock/api/question',
+    methods: "delete",
+    response: () => {
+      return{
+        error:0,
+        data: {
+          id: Random.id(),
+        }
+      }
+    }
   }
+  // Get questionnaire list
+  // {
+  //   url: '/mock/api/questionnaire/:id',
+  //   methods: "get",
+  //   response: () => {
+  //     return{
+  //       error:0,
+  //       data: {
+  //         id: Random.id(),
+  //         title: Random.ctitle(5, 10),
+  //         description: Random.csentence(10, 20),
+  //       }
+  //     }
+  //   }
+  // },
+  
+// Restful API
 ]
 export default arr
